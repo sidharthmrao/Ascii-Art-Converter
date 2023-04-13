fe62245dfaf5aedf514bfe1a3dbc7a92247c4705
Sidharth Rao <sidharthmrao@gmail.com>
2023-04-13 12:54:22 -0400

    Added color usage.

--- main.py
+++ main.py
@@ -4,34 +4,24 @@ image = "RickAstley2021.jpg"
 
 img = cv2.imread(f"./{image}")
 
-scale_percent = 3 # percent of original size
+scale_percent = 5  # percent of original size
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
   
 # resize image
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
  
-print('Resized Dimensions : ',resized.shape)
-
-grayImage = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
-print("HI")
-cv2.imwrite("./img_BW.jpg", blackAndWhiteImage)
-print(blackAndWhiteImage.shape)
+print('Resized Dimensions : ', resized.shape)
 
 ascii_shape = []
 
-for i in range(len(blackAndWhiteImage)):
+for i in range(len(resized)):
     ascii_shape.append("")
-    for j, val in enumerate(blackAndWhiteImage[i]):
-        if val == 255:
-            ascii_shape[i] += "- " # "- "
-        else:
-            ascii_shape[i] += "+ " # "+ "
+    for j, val in enumerate(resized[i]):
+        ascii_shape[i] += chr(val[0]//30 + 33)
+        print(chr(val[0] + 33), end="")
     ascii_shape[i] += "\n"
-
-print(ascii_shape)
 
 x = open("./ascii.txt", "w")
 x.writelines(ascii_shape)
