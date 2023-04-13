24d6a21d84dcae2ca7f9ff4f7ede45916e0d233a
Sidharth Rao <sidharthmrao@gmail.com>
2023-04-13 13:08:56 -0400

    Added stretching to image to account for font height width diff.

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ image = "RickAstley2021.jpg"
 
 img = cv2.imread(f"./{image}")
 
-scale_percent = 5  # percent of original size
+scale_percent = 3  # percent of original size
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
-  
+
 # resize image
 resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
- 
+
 print('Resized Dimensions : ', resized.shape)
 
 ascii_shape = []
@@ -19,8 +19,9 @@ ascii_shape = []
 for i in range(len(resized)):
     ascii_shape.append("")
     for j, val in enumerate(resized[i]):
+        if j % 2 == 0:  # Stretch by 50% to account for font width
+            ascii_shape[i] += chr(val[0] // 30 + 33)
         ascii_shape[i] += chr(val[0]//30 + 33)
-        print(chr(val[0] + 33), end="")
     ascii_shape[i] += "\n"
 
 x = open("./ascii.txt", "w")
